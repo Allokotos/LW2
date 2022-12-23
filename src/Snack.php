36686dea4f6e905d3fbc37714d8e433719a83d50
Alex M <tauren12rus@gmail.com>
2022-12-23 15:04:01 +0300

    initial commit

---
+++ src/Snack.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace lw2\src;
+
+abstract class Snack
+{
+    public string $name;
+    public string $chocolate;
+    public array $toppings;
+
+    public function __construct(string $name, string $choc, array $top)
+    {
+        $this->name = $name;
+        $this->chocolate = $choc;
+        $this->toppings = $top;
+    }
+
+    public function cutType(): string
+    {
+        $cutType = "По вертикали";
+        return $cutType;
+    }
+
+    public function prepare(): void
+    {
+        echo "Началось создание снэка {$this->name}";
+        echo "Добавляется шоколад {$this->chocolate}";
+        echo "Добавлены начинки:";
+        for ($i = 0; $i < count($this->toppings); $i++) {
+            echo "{$this->toppings[$i]}";
+        }
+    }
+}
